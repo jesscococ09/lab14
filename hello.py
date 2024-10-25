670adc913213fa424b23a491ef97fec420434e7c
jess <jesstorrealba@gmail.com>
2024-10-25 14:00:48 -0700

    add import from another file and update .gitignore

--- hello.py
+++ hello.py
@@ -1,13 +1,4 @@
-color_list= [(227,66,52),(205,96,144),(28,134,238),(72,209,204),(237,145,33),(250,250,70),(245,50,245),(100,231,231)]
-if color_list[7][0]>color_list[7][1] and color_list[7][0] > color_list[7][2]:
-    print('The color is reddish')
-elif color_list[7][1]>color_list[7][0] and color_list[7][1]>color_list[7][2]:
-    print('The color is greenish')
-elif color_list [7][0]==color_list [7][1]:
-    print('The color is a shade of yellow')
-elif color_list[7][0]==color_list [7][2]:
-    print('The color is a shade of magenta')
-elif color_list [7][1]==color_list[7][2]:
-    print('The color is a shade of cyan')
-else:
-    print("The color is bluish")
+from other_file import my_list
+
+print("Hello, World!")
+print(my_list)
